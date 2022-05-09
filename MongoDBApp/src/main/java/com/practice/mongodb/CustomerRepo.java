1cf4b9db7e985c54655cf1fa5274c38b25468aac
Komal Malhotra <104885087+kMalhotra1995@users.noreply.github.com>
2022-05-09 20:54:32 +0530

    Delete

--- MongoDBApp/src/main/java/com/practice/mongodb/CustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.practice.mongodb;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.practice.mongodb.Model.Customer;
-
-public interface CustomerRepo extends MongoRepository<Customer, String>{
-
-}
